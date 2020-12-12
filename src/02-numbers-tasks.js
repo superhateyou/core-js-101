e8ff8241807cb4ff587f438901f262cd9c3d1b6b
Pavel Serzhan <p.i.serzhan@gmail.com>
2020-12-12 21:27:08 +0300

    feat; some solutions

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -109,10 +109,8 @@ function getLinearEquationRoot(a, b) {
  *   (0,1) (0,1)     => 0
  *   (0,1) (1,2)     => 0
  */
-function getAngleBetweenVectors(x1, y1, x2, y2) {
-  const dotProduct = x1 * x2 + y1 * y2;
-  const vectorsLength = Math.sqrt(x1 ** 2 + y1 ** 2) * Math.sqrt(x2 ** 2 + y2 ** 2);
-  return Math.cos(dotProduct / vectorsLength);
+function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
+  throw new Error('Not implemented');
 }
 
 /**
