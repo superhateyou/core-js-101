b775405cffe440d6b4323a6b11bcc38a6ccec99e
Pavel Serzhan <p.i.serzhan@gmail.com>
2020-12-12 21:42:29 +0300

    feat; some solutions

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -52,7 +52,7 @@ function getCicleCircumference(radius) {
  *  -3, 3  => 0
  */
 function getAverage(value1, value2) {
-  return (value1 / 2 + value2 / 2);
+  return value1 / 2 + value2 / 2;
 }
 
 /**
@@ -87,7 +87,7 @@ function getDistanceBetweenPoints(x1, y1, x2, y2) {
  *   5*x = 0         => 0
  */
 function getLinearEquationRoot(a, b) {
-  return -b / a;
+  throw new Error();
 }
 
 
@@ -109,12 +109,9 @@ function getLinearEquationRoot(a, b) {
  *   (0,1) (0,1)     => 0
  *   (0,1) (1,2)     => 0
  */
-function getAngleBetweenVectors(x1, y1, x2, y2) {
-  const dotProduct = x1 * x2 + y1 * y2;
-  const vectorsLength = Math.sqrt(x1 ** 2 + y1 ** 2) * Math.sqrt(x2 ** 2 + y2 ** 2);
-  return Math.acos(dotProduct / vectorsLength);
+function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
+  throw new Error('Not implemented');
 }
-
 
 /**
  * Returns a last digit of a integer number.
@@ -128,8 +125,8 @@ function getAngleBetweenVectors(x1, y1, x2, y2) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(value) {
-  return value % 10;
+function getLastDigit(/* value */) {
+  throw new Error('Not implemented');
 }
 
 
@@ -144,8 +141,8 @@ function getLastDigit(value) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(value) {
-  return +value;
+function parseNumberFromString(/* value */) {
+  throw new Error('Not implemented');
 }
 
 /**
