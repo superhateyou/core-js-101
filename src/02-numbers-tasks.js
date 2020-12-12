e56fcec2b539d1fcac31492ce90f7adcf5501647
Pavel Serzhan <p.i.serzhan@gmail.com>
2020-12-12 20:59:58 +0300

    feat; some solutions

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -52,7 +52,7 @@ function getCicleCircumference(radius) {
  *  -3, 3  => 0
  */
 function getAverage(value1, value2) {
-  return ((value1 + value2) / 2);
+  return value1 / 2 + value2 / 2;
 }
 
 /**
