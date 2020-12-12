21f72dbe681ccc80d7b3bd368ee167965e7a0349
Pavel Serzhan <p.i.serzhan@gmail.com>
2020-12-12 21:30:12 +0300

    feat; some solutions

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -109,9 +109,12 @@ function getLinearEquationRoot(a, b) {
  *   (0,1) (0,1)     => 0
  *   (0,1) (1,2)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  const dotProduct = x1 * x2 + y1 * y2;
+  const vectorsLength = Math.sqrt(x1 ** 2 + y1 ** 2) * Math.sqrt(x2 ** 2 + y2 ** 2);
+  return Math.acos(dotProduct / vectorsLength);
 }
+
 
 /**
  * Returns a last digit of a integer number.
